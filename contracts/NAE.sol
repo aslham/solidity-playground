85fa0974e7af43807042f209643bc1a6df0f1dc0
Aslham <116582001+aslham@users.noreply.github.com>
2026-05-03 12:40:28 +0000

    add mint function to NAE

--- contracts/NAE.sol
+++ contracts/NAE.sol
@@ -10,3 +10,10 @@ contract NAE {
 // refactor cleanup pass
 
 event Mint(address indexed to, uint256 amount);
+
+mapping(address => uint256) public balance;
+
+function mint(address to, uint256 amount) public {
+    balance[to] += amount;
+    emit Mint(to, amount);
+}
