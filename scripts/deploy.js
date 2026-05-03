14aff025b2815a277c7c832634a0d4224bc949bb
Aslham <116582001+aslham@users.noreply.github.com>
2026-05-03 20:16:45 +0000

    add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,7 @@
-// deploy scripts placeholder
+async function main() {
+    const MOM = await ethers.getContractFactory("MOM");
+    const mom = await MOM.deploy();
+    console.log("MOM deployed to:", mom.target);
+}
+
+main();
